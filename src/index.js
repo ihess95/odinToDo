16bbbe6db47a6920dd042ac5b048408b5b536355
Ian Hess <ian.h.hess@gmail.com>
2022-12-07 09:43:54 -0500

    create card builder mod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { todoConst } from "./todoConst";
+import { cardConstructor } from "./cardConstructor";
 
-const bodyDiv = document.querySelector(".bodyContainer");
-const testDiv = document.createElement("div");
-const cardOne = new todoConst("test", "testing", "today", "eh", "testing dude");
-testDiv.textContent = cardOne.cardTitle;
-testDiv.classList.add("test");
-bodyDiv.appendChild(testDiv);
+cardConstructor(
+  "Finish this task app",
+  "Gotta work on it dude",
+  "soon",
+  "pretty high",
+  "working on it"
+);
