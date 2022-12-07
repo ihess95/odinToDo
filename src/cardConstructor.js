ea2c2d7906e996b5c3d04f234f7957dbacb4cc46
Ian Hess <ian.h.hess@gmail.com>
2022-12-07 14:07:39 -0500

    add all basic fields for cards

--- src/cardConstructor.js
+++ src/cardConstructor.js
@@ -5,13 +5,28 @@ function cardConstructor(hiddenTitle, title, desc, dueDate, priority, notes) {
   const bodyDiv = document.querySelector(".bodyContainer");
   const cardDiv = document.createElement("div");
   const cardTitleDiv = document.createElement("div");
+  cardTitleDiv.classList.add("title");
   const cardDescDiv = document.createElement("div");
+  cardDescDiv.classList.add("desc");
+  const cardDueDateDiv = document.createElement("div");
+  cardDueDateDiv.classList.add("dueDate");
+  const cardPriorityDiv = document.createElement("div");
+  cardPriorityDiv.classList.add("priority");
+  const cardNotesDiv = document.createElement("div");
+  cardNotesDiv.classList.add("notes");
   const cardInst = new todoConst(title, desc, dueDate, priority, notes);
   cardTitleDiv.textContent = cardInst.cardTitle;
+  cardDescDiv.textContent = cardInst.cardDesc;
+  cardDueDateDiv.textContent = cardInst.cardDueDate;
+  cardPriorityDiv.textContent = cardInst.cardPriority;
+  cardNotesDiv.textContent = cardInst.cardDesc;
   cardDiv.classList.add(cardClass);
-  bodyDiv.appendChild(cardDiv);
   cardDiv.appendChild(cardTitleDiv);
   cardDiv.appendChild(cardDescDiv);
+  cardDiv.appendChild(cardDueDateDiv);
+  cardDiv.appendChild(cardPriorityDiv);
+  cardDiv.appendChild(cardNotesDiv);
+  bodyDiv.appendChild(cardDiv);
 }
 
 export { cardConstructor };
