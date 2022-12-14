03a0d3b3bd821f46ad2592eedbd9719682981d3e
Ian Hess <ian.h.hess@gmail.com>
2022-12-14 09:43:49 -0500

    store new prompt info

--- src/newTaskPrompt.js
+++ src/newTaskPrompt.js
@@ -25,15 +25,23 @@ function createPrompt() {
 
   const priorityBtnCont = document.createElement("div");
 
+  let priorityField = "";
+
+  function priority(input) {
+    priorityField = btn.textContent;
+  }
   const lowBtn = document.createElement("button");
   lowBtn.textContent = "Low Priority";
   lowBtn.classList.add("lowBtn");
+  lowBtn.addEventListener("click", priority(lowBtn));
   const midBtn = document.createElement("button");
   midBtn.textContent = "Medium Priority";
   midBtn.classList.add("midBtn");
+  midBtn.addEventListener("click", priority(midBtn));
   const hiBtn = document.createElement("button");
   hiBtn.textContent = "High Priority";
   hiBtn.classList.add("hiBtn");
+  hiBtn.addEventListener("click", priority(hiBtn));
 
   priorityBtnCont.classList.add("priorityBtnCont");
 
@@ -42,6 +50,16 @@ function createPrompt() {
   const confirmBtn = document.createElement("button");
   confirmBtn.textContent = "Create Task";
   confirmBtn.classList.add("confirmBtn");
+  confirmBtn.addEventListener("click", function () {
+    // const newTitle = titleField.value
+    const newCard = new NewCardInput(
+      titleField.value,
+      descField.value,
+      dateField.value,
+      priorityField
+    );
+    console.log(newCard);
+  });
   const denyBtn = document.createElement("button");
   denyBtn.textContent = "Cancel";
   denyBtn.classList.add("denyBtn");
